2de6e39b87b83d30fb0d0b5931e33058b43a6cb1
Nguyễn Văn Hải <nguyenhai20112003@gmail.com>
2024-02-19 15:11:13 +0700

    contact

---
+++ page/contact/index.js
@@ -0,0 +1,58 @@
+function home() {
+  window.location.href = "https://viracresearch.com/trang-chu/";
+}
+
+function changeLang(lang) {
+  langSelect = lang;
+  const elements = document.querySelectorAll("[data-lang]");
+  elements.forEach((element) => {
+    const translations = JSON.parse(element.getAttribute("data-lang"));
+    if (translations[lang]) {
+      element.textContent = translations[lang];
+    }
+  });
+  const languageIcon = document.getElementById("languageIcon");
+  if (lang === "vi") {
+    languageIcon.src = "./../../image/vi.png";
+  } else if (lang === "en") {
+    languageIcon.src = "./../../image/en.png";
+  }
+  const elementsInput = document.querySelectorAll("[data-translate]");
+  elementsInput.forEach((element) => {
+    const key = element.dataset.translate;
+    element.placeholder = translations[lang][key];
+  });
+  closeModal();
+}
+
+function openModal() {
+  const modal = document.querySelector(".modal");
+  modal.style.display = "block";
+}
+function closeModal() {
+  const modal = document.querySelector(".modal");
+  modal.style.display = "none";
+}
+
+function news() {
+  window.location.href = "http://online.gov.vn/Home/WebDetails/86982?AspxAutoDetectCookieSupport=1";
+}
+$('.openmodale').click(function (e) {
+  e.preventDefault();
+  $('.modale_disclaimer').addClass('opened');
+});
+$('.closemodale').click(function (e) {
+  e.preventDefault();
+  $('.modale_disclaimer').removeClass('opened');
+});
+
+
+$('.confidentiality').click(function (e) {
+  e.preventDefault();
+  $('.modale_confidentiality').addClass('opened');
+});
+$('.closemodale').click(function (e) {
+  e.preventDefault();
+  $('.modale_confidentiality').removeClass('opened');
+});
+
