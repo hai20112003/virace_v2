b21d77384e63fbcb66f65dc3dae992f04c638811
Nguyễn Văn Hải <nguyenhai20112003@gmail.com>
2024-02-29 11:09:20 +0700

    update

--- page/logout/index.js
+++ page/logout/index.js
@@ -1,67 +1,9 @@
-const email = document.getElementById("email");
+const confirmPassword = document.getElementById("confirmPassword");
 const password = document.getElementById("password");
-const formLogin = document.getElementById("formLogin");
+const formChange = document.getElementById('formChange');
+const lang = localStorage.getItem("lang");
 let open_modal_lang = true;
 let langSelect = "vi";
-const lang = localStorage.getItem("lang");
-function showError(input, messageKey, lang) {
-  const formControl = input.closest(".form-control-input");
-  formControl.className = "form-control-input error";
-  const small = formControl.parentElement.querySelector("small");
-  console.log(small);
-  small.innerText = getTranslatedMessage(messageKey, lang);
-}
-function getTranslatedMessage(key, lang) {
-  const translation = translations[lang];
-  return translation ? translation[key] || key : key;
-}
-function showSucces(input) {
-  const formControl = input.parentElement;
-  formControl.className = "form-control-input success";
-}
-
-function checkEmail(input) {
-  const re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    console.log(re.test(input.value.trim()));
-  
-    if (re.test(input.value.trim())) {
-    showSucces(input);
-  } else {
-    console.log(1);
-    showError(input, "email_invalid", langSelect);
-  }
-}
-function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-function checkRequired(inputArr) {
-  inputArr.forEach(function (input) {
-    if (input.value.trim() === "") {
-      showError(input, "required_field", langSelect);
-    } else {
-      showSucces(input);
-    }
-  });
-}
-formLogin.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const smallElement = document.getElementsByClassName("error");
-  smallElement.innerText = "";
-  checkRequired([email, password]);
-  checkEmail(email);
-});
-function clickIsChecked(isChecked) {
-  const falseElement = document.querySelector(".false");
-  const trueElement = document.querySelector(".true");
-  if (isChecked) {
-    falseElement.style.display = "none";
-    trueElement.style.display = "block";
-  } else {
-    falseElement.style.display = "block";
-    trueElement.style.display = "none";
-  }
-}
 
 function home() {
   window.location.href = "https://viracresearch.com/trang-chu/";
@@ -72,7 +14,6 @@ function changeLang(lang) {
   const elements = document.querySelectorAll("[data-lang]");
   elements.forEach((element) => {
     const translations = JSON.parse(element.getAttribute("data-lang"));
-
     if (translations[lang]) {
       element.textContent = translations[lang];
     }
@@ -107,36 +48,29 @@ function closeModal() {
   modal.style.display = "none";
 }
 
-const translations = {
-  en: {
-    email_placeholder: "Enter your email",
-    password_placeholder: "Enter your password",
-    email_invalid: "Email is not valid",
-    required_field: "Field is required",
-  },
-  vi: {
-    email_placeholder: "Nhập email của bạn",
-    password_placeholder: "Nhập mật khẩu của bạn",
-    email_invalid: "Email không hợp lệ",
-    required_field: "Vui lòng nhập giá trị",
-  },
-};
 
-function togglePasswordType() {
-  const passwordInput = document.getElementById("password");
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
-  } else {
-    passwordInput.type = "password";
-  }
-}
+
 function news() {
-  window.location.href =
-    "http://online.gov.vn/Home/WebDetails/86982?AspxAutoDetectCookieSupport=1";
+  window.location.href = "http://online.gov.vn/Home/WebDetails/86982?AspxAutoDetectCookieSupport=1";
 }
-
 if (lang) {
   changeLang(lang);
 } else {
   changeLang("vi");
+}
+
+
+modal_logout = true;
+function close_modal_true() {
+  const modal = document.querySelector("#modal_logout");
+  modal.style.display = "none";
+}
+function open_modal_logout_true() {
+  if (modal_logout) {
+    const modal = document.querySelector("#modal_logout");
+    modal.style.display = "block";
+    setTimeout(function() {
+      window.location.href = "http://127.0.0.1:5500/page/login/index.html";
+    }, 2000);
+  }
 }
